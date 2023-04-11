5bcf0cdc42b9c7b4c99407f40640d059ed35b9b9
Qusai <qalnaabi1@sheffield.ac.uk>
2023-04-11 06:50:57 +0100

    fix the upload files api --it works fine now

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const multer = require('multer');
+const Multer = require('multer');
 const { Storage } = require('@google-cloud/storage');
 const fs = require('fs');
-const upload = multer();
 const bodyParser = require('body-parser');
 const Pdf_data = require('./models/pdf_data')
 const pdf = require('pdf-parse');
@@ -43,6 +42,63 @@ const storage = new Storage({
 });
 const bucket = storage.bucket(process.env.BUCKET);
 
+// Initialize Multer
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+});
+
+app.post('/upload', multer.array('files'), async (req, res) => {
+  const files = req.files;
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const fileName = Date.now() + '-' + file.originalname;
+    const fileBuffer = file.buffer;
+
+    // Create a new blob in the bucket and upload the file data.
+    const blob = bucket.file(fileName);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+      contentType: file.mimetype,
+    });
+
+    blobStream.on('error', (err) => {
+      console.log(err);
+      res.status(500).send({ message: 'Error uploading file.' });
+    });
+
+    blobStream.on('finish', async () => {
+      // Parse the PDF file
+      const data = await pdf(fileBuffer);
+      const lines = [];
+      data.text.split('\n').forEach(line => {
+        lines.push(line);
+      });
+
+      // Drop empty lines from the list
+      const nonEmptyLines = lines.filter(line => line.trim() !== '');
+      const fileSizeInKB = file.size / 1024;
+
+      // Create a new Pdf_data instance with the filename, current date, and lines
+      const pdfData = new Pdf_data({
+        fileName: fileName,
+        createdAt: new Date(),
+        pages_number: data.numpages,
+        sentences: nonEmptyLines,
+        size: fileSizeInKB.toFixed(2),
+      });
+
+      // Save the Pdf_data instance to the database
+      await pdfData.save();
+    });
+
+    blobStream.end(fileBuffer);
+  }
+
+  res.status(200).send(`${files.length} files uploaded successfully.`);
+});
+
+
 // Retrieve a stored PDF given the ID
 app.get('/file/:id', getParams,async (req, res) => {
   try {
@@ -63,74 +119,6 @@ app.get('/file/:id', getParams,async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).send('An error occurred while retrieving the file.');
-  }
-});
-
-// add files
-app.post('/addFiles', async (req, res) => {
-  try {
-    // Get a list of all the PDF files in the folder
-    const folderPath = 'pdf_folder';
-    const files = await fs.promises.readdir(folderPath);
-    const filesCount = files.length;
-    let filesProcessed = 0;
-
-    // Loop through each file in the folder and upload it
-    files.forEach(async (file) => {
-      const filePath = `${folderPath}/${file}`;
-      const fileContent = await fs.promises.readFile(filePath);
-      const filename = `${file}`;
-
-      // Create a write stream to upload the file
-      const storageFile = bucket.file(filename);
-      const stream = storageFile.createWriteStream({
-        metadata: {
-          contentType: 'application/pdf',
-        },
-      });
-
-      // Handle successful upload
-      stream.on('finish', () => {
-        // Parse the PDF file
-        pdf(fileContent).then(function(data) {
-          const lines = [];
-          data.text.split('\n').forEach(line => {
-            lines.push(line);
-          });
-
-          // Drop empty lines from the list
-          const nonEmptyLines = lines.filter(line => line.trim() !== '');
-          const stats = fs.statSync(filePath);
-          const fileSizeInKB = stats.size / 1024;
-
-          // Create a new Pdf_data instance with the filename, current date, and lines
-          const pdfData = new Pdf_data({
-            fileName: filename,
-            createdAt: new Date(),
-            pages_number: data.numpages,
-            sentences: nonEmptyLines,
-            size :fileSizeInKB.toFixed(2)
-          });
-
-          // Save the Pdf_data instance to the database
-          pdfData.save();
-
-          // Increment the number of files processed and check if all files have been processed
-          filesProcessed++;
-          if (filesProcessed === filesCount) {
-            // Send a response indicating that all files have been uploaded
-            res.status(200).send(`${filesCount} files uploaded successfully.`);
-          }
-        });
-      });
-
-      // Pipe the file data to the write stream
-      stream.end(fileContent);
-    });
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("An error occurred while uploading the files.");
   }
 });
 
@@ -298,3 +286,110 @@ async function getParams(req, res, next) {
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
+
+// add files
+// app.post('/addFiles', async (req, res) => {
+//   try {
+//     // Get a list of all the PDF files in the folder
+//     const folderPath = 'pdf_folder';
+//     const files = await fs.promises.readdir(folderPath);
+//     const filesCount = files.length;
+//     let filesProcessed = 0;
+
+//     // Loop through each file in the folder and upload it
+//     files.forEach(async (file) => {
+//       const filePath = `${folderPath}/${file}`;
+//       const fileContent = await fs.promises.readFile(filePath);
+//       const filename = `${file}`;
+
+//       // Create a write stream to upload the file
+//       const storageFile = bucket.file(filename);
+//       const stream = storageFile.createWriteStream({
+//         metadata: {
+//           contentType: 'application/pdf',
+//         },
+//       });
+
+//       // Handle successful upload
+//       stream.on('finish', () => {
+//         // Parse the PDF file
+//         pdf(fileContent).then(function(data) {
+//           const lines = [];
+//           data.text.split('\n').forEach(line => {
+//             lines.push(line);
+//           });
+
+//           // Drop empty lines from the list
+//           const nonEmptyLines = lines.filter(line => line.trim() !== '');
+//           const stats = fs.statSync(filePath);
+//           const fileSizeInKB = stats.size / 1024;
+
+//           // Create a new Pdf_data instance with the filename, current date, and lines
+//           const pdfData = new Pdf_data({
+//             fileName: filename,
+//             createdAt: new Date(),
+//             pages_number: data.numpages,
+//             sentences: nonEmptyLines,
+//             size :fileSizeInKB.toFixed(2)
+//           });
+
+//           // Save the Pdf_data instance to the database
+//           pdfData.save();
+
+//           // Increment the number of files processed and check if all files have been processed
+//           filesProcessed++;
+//           if (filesProcessed === filesCount) {
+//             // Send a response indicating that all files have been uploaded
+//             res.status(200).send(`${filesCount} files uploaded successfully.`);
+//           }
+//         });
+//       });
+
+//       // Pipe the file data to the write stream
+//       stream.end(fileContent);
+//     });
+
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("An error occurred while uploading the files.");
+//   }
+// });
+
+
+// // Retrieve a stored PDF given the ID
+// app.get('/file/:id', getParams, async (req, res) => {
+//   try {
+//     const name = res.file.fileName;
+//     const [files] = await bucket.getFiles();
+
+//     // Find the file with the given name
+//     const file = files.find(file => file.name === name);
+
+//     if (!file) {
+//       // Return a 404 response if the file is not found
+//       return res.status(404).send('File not found.');
+//     }
+
+//     // Get a readable stream for the file and pipe it to a local file
+//     const stream = file.createReadStream();
+//     const filePath = `./pdf_files/${name}`;
+//     const fileWriteStream = fs.createWriteStream(filePath);
+//     stream.pipe(fileWriteStream);
+
+//     // Handle successful write to file
+//     fileWriteStream.on('finish', () => {
+//       res.status(200).send(`File written to ${filePath} successfully.`);
+//     });
+
+//     // Handle errors writing to file
+//     fileWriteStream.on('error', (err) => {
+//       console.error(err);
+//       res.status(500).send('An error occurred while writing the file.');
+//     });
+
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('An error occurred while retrieving the file.');
+//   }
+// });
